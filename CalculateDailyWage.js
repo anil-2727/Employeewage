b3151ee5c537f2d6fa2e2640724f66df0216dcdd
anil-2727 <bali39959@gmail.com>
2021-09-01 09:25:46 +0530

    wage of month

--- CalculateDailyWage.js
+++ CalculateDailyWage.js
@@ -1,10 +1,14 @@
-//solving using case statement
+//wage of a month
 const fullTime = 1;
-const emp_rate_per_hour = 20;
 const partTime = 2;
 const absent = 0;
-let empHrs;
+const emp_rate_per_hour = 20;
+const num_of_working_days = 20;
+let empHrs = 0;
+let totalEmpWage = 0;
+let empWage;
 function employee (){
+    for(let day = 0; day < num_of_working_days ; day++){
 let empCheck = Math.floor((Math.random() * 10) % 3);
 switch ( empCheck ){
     case fullTime:
@@ -16,9 +20,10 @@ switch ( empCheck ){
     default:
         empHrs = 0;
 }
-let empWage = empHrs * emp_rate_per_hour;
-console.log("Daily Employee Wage = " + empWage);
+empWage = empHrs * emp_rate_per_hour;
+console.log(empWage);
+totalEmpWage += empWage;
+}
+console.log(totalEmpWage);
 }
 employee();
-
-
