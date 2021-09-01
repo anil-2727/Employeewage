b3c581b5a78e64777acac0de5e10dbf3926c2c5d
anil-2727 <bali39959@gmail.com>
2021-09-01 09:21:55 +0530

    using case statement

--- CalculateDailyWage.js
+++ CalculateDailyWage.js
@@ -1,4 +1,4 @@
-// Add Part time Employee & Wage
+//solving using case statement
 const fullTime = 1;
 const emp_rate_per_hour = 20;
 const partTime = 2;
@@ -6,16 +6,19 @@ const absent = 0;
 let empHrs;
 function employee (){
 let empCheck = Math.floor((Math.random() * 10) % 3);
-if (empCheck == fullTime ){
-     empHrs = 8;
-}
-else if (empCheck == partTime ){
-    empHrs = 4;
-}
-else{
-    console.log("Employe is absent");
+switch ( empCheck ){
+    case fullTime:
+        empHrs = 8;
+        break;
+    case partTime:
+        empHrs = 4;
+        break;
+    default:
+        empHrs = 0;
 }
 let empWage = empHrs * emp_rate_per_hour;
 console.log("Daily Employee Wage = " + empWage);
 }
 employee();
+
+
