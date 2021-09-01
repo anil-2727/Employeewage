28c67710ec1f66cc8b8c127590965239d8451f58
anil-2727 <bali39959@gmail.com>
2021-09-01 09:15:54 +0530

    part time employee added

--- CalculateDailyWage.js
+++ CalculateDailyWage.js
@@ -1,13 +1,21 @@
-//To check daily wage of employee
+// Add Part time Employee & Wage
 const fullTime = 1;
 const emp_rate_per_hour = 20;
+const partTime = 2;
 const absent = 0;
-let empCheck = Math.floor((Math.random() * 10) % 2);
+let empHrs;
+function employee (){
+let empCheck = Math.floor((Math.random() * 10) % 3);
 if (empCheck == fullTime ){
-    let empHrs = 8;
-    let empWage = empHrs * emp_rate_per_hour;
-    console.log("Daily Employee Wage = " + empWage);
+     empHrs = 8;
+}
+else if (empCheck == partTime ){
+    empHrs = 4;
 }
 else{
     console.log("Employe is absent");
 }
+let empWage = empHrs * emp_rate_per_hour;
+console.log("Daily Employee Wage = " + empWage);
+}
+employee();
